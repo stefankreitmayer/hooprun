f2e0b7595aeee0d063d41e69a67fbb8a318ea2c7
Stefan Kreitmayer <stefankreitmayer@gmail.com>
2015-05-20 13:31:00 +0200

    comment out unneeded test templates from scaffold

--- spec/controllers/runs_controller_spec.rb
+++ spec/controllers/runs_controller_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe RunsController, type: :controller do
   let(:valid_attributes) {
   }
 
-  let(:invalid_attributes) {
-  }
+  # let(:invalid_attributes) {
+  # }
 
   let(:valid_session) { {} }
 
@@ -61,57 +61,57 @@ RSpec.describe RunsController, type: :controller do
       end
     end
 
-    context "with invalid params" do
-      it "assigns a newly created but unsaved run as @run" do
-        post :create, {:run => invalid_attributes}, valid_session
-        expect(assigns(:run)).to be_a_new(Run)
-      end
+    # context "with invalid params" do
+    #   it "assigns a newly created but unsaved run as @run" do
+    #     post :create, {:run => invalid_attributes}, valid_session
+    #     expect(assigns(:run)).to be_a_new(Run)
+    #   end
 
-      it "re-renders the 'new' template" do
-        post :create, {:run => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
+    #   it "re-renders the 'new' template" do
+    #     post :create, {:run => invalid_attributes}, valid_session
+    #     expect(response).to render_template("new")
+    #   end
+    # end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+  # describe "PUT #update" do
+  #   context "with valid params" do
+  #     let(:new_attributes) {
+  #       skip("Add a hash of attributes valid for your model")
+  #     }
 
-      it "updates the requested run" do
-        run = Run.create! valid_attributes
-        put :update, {:id => run.to_param, :run => new_attributes}, valid_session
-        run.reload
-        skip("Add assertions for updated state")
-      end
+  #     it "updates the requested run" do
+  #       run = Run.create! valid_attributes
+  #       put :update, {:id => run.to_param, :run => new_attributes}, valid_session
+  #       run.reload
+  #       skip("Add assertions for updated state")
+  #     end
 
-      it "assigns the requested run as @run" do
-        run = Run.create! valid_attributes
-        put :update, {:id => run.to_param, :run => valid_attributes}, valid_session
-        expect(assigns(:run)).to eq(run)
-      end
+  #     it "assigns the requested run as @run" do
+  #       run = Run.create! valid_attributes
+  #       put :update, {:id => run.to_param, :run => valid_attributes}, valid_session
+  #       expect(assigns(:run)).to eq(run)
+  #     end
 
-      it "redirects to the run" do
-        run = Run.create! valid_attributes
-        put :update, {:id => run.to_param, :run => valid_attributes}, valid_session
-        expect(response).to redirect_to(run)
-      end
-    end
+  #     it "redirects to the run" do
+  #       run = Run.create! valid_attributes
+  #       put :update, {:id => run.to_param, :run => valid_attributes}, valid_session
+  #       expect(response).to redirect_to(run)
+  #     end
+  #   end
 
-    context "with invalid params" do
-      it "assigns the run as @run" do
-        run = Run.create! valid_attributes
-        put :update, {:id => run.to_param, :run => invalid_attributes}, valid_session
-        expect(assigns(:run)).to eq(run)
-      end
+  #   context "with invalid params" do
+  #     it "assigns the run as @run" do
+  #       run = Run.create! valid_attributes
+  #       put :update, {:id => run.to_param, :run => invalid_attributes}, valid_session
+  #       expect(assigns(:run)).to eq(run)
+  #     end
 
-      it "re-renders the 'edit' template" do
-        run = Run.create! valid_attributes
-        put :update, {:id => run.to_param, :run => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
-      end
-    end
-  end
+  #     it "re-renders the 'edit' template" do
+  #       run = Run.create! valid_attributes
+  #       put :update, {:id => run.to_param, :run => invalid_attributes}, valid_session
+  #       expect(response).to render_template("edit")
+  #     end
+  #   end
+  # end
 end
